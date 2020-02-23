9792cc66204381397e88ba10d2c502eecc23b0d4
Martin Wong <32690246+cobras1077@users.noreply.github.com>
2020-02-22 16:39:34 -0800

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+        var HP=100;
 
-
+ 
     $(".Start").click(function(){
         // $("body").css("background-image": "url('https://pngimage.net/wp-content/uploads/2018/06/pokemon-background-png-4.png');");
         // $("body").css("background-color", "yellow");
+        // $("#empty").append("#empty");
+        $("#hp").append("<p> Hp: "+HP+"</p>");
+        
         var search=$("#chosenone").val();
         var customurl="https://pokeapi.co/api/v2/pokemon/"+search+"/";
         $(document).ready(function(){
@@ -11,11 +15,31 @@
             method:"GET",
             success:function(response){
                  $("#Image").append("<img class='image' src='"+response.sprites.back_shiny+"'>");
-                 $("#move1").append(response.moves[0].move.name);
-                 $("#move2").append(response.moves[1].move.name);
-                 $("#move3").append(response.moves[2].move.name);
-                 $("#move4").append(response.moves[3].move.name);
+                 $("#move1o").append("<p id='move1'>"+response.moves[0].move.name+"</p>");
+                 $("#move2o").append("<p id='move2'>"+response.moves[1].move.name+"</p>");
+                 $("#move3o").append("<p id='move3'>"+response.moves[2].move.name+"</p>");
+                 $("#move4o").append("<p id='move4'>"+response.moves[3].move.name+"</p>");
             }
         }) ; 
     }) ;  
+    });
+    $("#move1o").click(function(){
+
+        var newHP=HP--;
+       $("#hp").html("<p> Hp: "+newHP+"</p>");
+    });
+     $("#move2o").click(function(){
+
+        var newHP=HP--;
+       $("#hp").html("<p> Hp: "+newHP+"</p>");
+    });
+     $("#move3o").click(function(){
+
+        var newHP=HP--;
+       $("#hp").html("<p> Hp: "+newHP+"</p>");
+    });
+     $("#move4o").click(function(){
+
+        var newHP=HP--;
+       $("#hp").html("<p> Hp: "+newHP+"</p>");
     });
